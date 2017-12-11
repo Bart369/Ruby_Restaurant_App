05e93051ec0b73ce91d0f30f90129a243b2e2845
bart369 <bartinpeguero@gmail.com>
2017-12-11 14:16:29 -0500

    added users routes and controller

--- Restaurant_App/config/routes.rb
+++ Restaurant_App/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'restaurants#index'
   resources :restaurants
 
+  # could also include index, i guess if there was a reason to show all users?
+  resources :users, only: [:new, :create, :show]
+
   # resource not resources
   resource :session, only: [:new, :create, :destroy] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
