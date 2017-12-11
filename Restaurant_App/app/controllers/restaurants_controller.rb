166dba9c2bfac11eede557898223e45ba2b6726b
bart369 <bartinpeguero@gmail.com>
2017-12-11 17:25:31 -0500

    crud works for different users!

--- Restaurant_App/app/controllers/restaurants_controller.rb
+++ Restaurant_App/app/controllers/restaurants_controller.rb
@@ -18,7 +18,7 @@ class RestaurantsController < ApplicationController
 
     def create
         @restaurant = Restaurant.new(restaurant_params)
-        # @restaurant.user = current_user
+        @restaurant.user = current_user
 
         if @restaurant.save
             flash[:notice] = 'Restaurant added to list!'
